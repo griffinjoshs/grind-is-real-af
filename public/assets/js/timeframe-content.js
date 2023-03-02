8d9fb5312d08ceee4f1ee2cdb38a87e156119267
griffinjoshs <leveloutusa@gmail.com>
2023-03-02 12:49:37 -0500

    added timing upon display

--- public/assets/js/timeframe-content.js
+++ public/assets/js/timeframe-content.js
@@ -1,8 +1,9 @@
 // dailyview navtab bar (today, this week, this month, this year)
+const pills = document.querySelectorAll(".nav-pills .nav-link");
+
 const dayContent = () => {
 document.querySelector('.day-content').classList.remove('hide')
 
-    const pills = document.querySelectorAll(".nav-pills .nav-link");
     pills.forEach((pill) => {
       pill.addEventListener("click", (e) => {
         e.preventDefault();
@@ -23,3 +24,23 @@ document.querySelector('.day-content').classList.remove('hide')
     });
   }
  
+// get current hour
+const currentHour = new Date().getHours();
+
+// get time content elements
+const timeContent = document.querySelector('.time-content');
+const tabs = timeContent.querySelectorAll('.nav-link');
+const tabPanes = timeContent.querySelectorAll('.tab-pane');
+
+// activate appropriate tab based on current time
+if (currentHour < 12) {
+  tabs[0].classList.add('active');
+  tabPanes[0].classList.add('show', 'active');
+} else if (currentHour < 18) {
+  tabs[1].classList.add('active');
+  tabPanes[1].classList.add('show', 'active');
+} else {
+  tabs[2].classList.add('active');
+  tabPanes[2].classList.add('show', 'active');
+}
+
